c258e6975c85865cf660d4fcf0063d08b8e31ad3
Peter Bodizs <peter.bodizs@tekenable.com>
2025-12-02 16:26:40 +0100

    Day 1 b solution

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -8,29 +8,44 @@ var zeroPositions = 0;
 
 foreach (string line in File.ReadLines(filePath))
 {
+    Console.WriteLine($"Position: {pos}");
     Console.WriteLine(line);
 
     var dir = line[0];
     var dist = int.Parse(line[1..]);
+    
+    zeroPositions += dist / 100;
+    Console.WriteLine($"Full Circles: {dist / 100}");
+    dist %= 100;
+
     if (dir == 'R')
-    {
+    {        
         pos += dist;
-        
+        if (pos >= 100)
+        {            
+            zeroPositions++;
+            Console.WriteLine($"Full Circles++ {zeroPositions}");
+        }
     }
     else if (dir == 'L')
     {
+        var origPos = pos;
         pos -= dist;
+        if (origPos != 0 && pos <= 0)
+        {            
+            zeroPositions++;
+            Console.WriteLine($"Full Circles++ {zeroPositions}");
+        }
     }
 
     pos %= 100;
-    //pos = pos > 0 ? pos : pos + 100;
+    pos = pos >= 0 ? pos : pos + 100;
 
-    if (pos == 0)
-    {
-        zeroPositions++;
-    }
-
-    Console.WriteLine($"Position: {pos}");
+    //Day 1 a calculations
+    //if (pos == 0)
+    //{
+    //    zeroPositions++;
+    //}   
 }
 
 Console.WriteLine($"Day 1a answer; Zero positions: {zeroPositions}");
