fe117349d844ad8b79a144ea8210a78354950df2
Peter Bodizs <peter.bodizs@tekenable.com>
2025-12-04 10:42:20 +0100

    day 4 a solution

---
+++ Day4/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Day 4");
+Console.WriteLine();
+
+string filePath = "input.txt";
+var answer = 0;
+var rollOfPapers = await File.ReadAllLinesAsync(filePath);
+var boundaryRow = new string('.', rollOfPapers.First().Length);
+rollOfPapers = [.. rollOfPapers.Prepend(boundaryRow)];
+rollOfPapers = [.. rollOfPapers.Append(boundaryRow)];
+
+rollOfPapers = rollOfPapers.Select(line => $".{line}.").ToArray();
+
+for (int row = 1; row < rollOfPapers.Length - 1; row++)
+{
+    for (int col = 1; col < rollOfPapers.First().Length - 1; col++)
+    {
+        Console.Write(rollOfPapers[row][col]);
+        if (rollOfPapers[row][col] == '@')
+        {
+            var adjacentRolls = rollOfPapers[row - 1][(col - 1)..(col + 2)].Count(c => c == '@');
+            adjacentRolls += (rollOfPapers[row][(col - 1)..(col + 2)].Count(c => c == '@') - 1);
+            adjacentRolls += rollOfPapers[row + 1][(col - 1)..(col + 2)].Count(c => c == '@');
+
+            if (adjacentRolls < 4)
+            {
+                answer++;
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine($"Day 4 answer: {answer}");
