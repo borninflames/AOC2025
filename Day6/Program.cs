19c536d3aeeedd2487b0e8a6ee081d2715a7382a
Peter Bodizs <peter.bodizs@tekenable.com>
2025-12-07 13:40:53 +0100

    day 7 part 1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -3,23 +3,28 @@ Console.WriteLine();
 
 string filePath = "input.txt";
 var rawInput = await File.ReadAllLinesAsync(filePath);
+var rotatedInput = new string[rawInput[0].Length];
 List<KeyValuePair<char, List<long>>> columns = [];
-
-var ops = rawInput[rawInput.Length - 1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToList();
-
-for (var i = 0; i < ops.Count; i++)
-{
-    columns.Add(new KeyValuePair<char, List<long>>(ops[i], []));
-}
-
-for (var i = 0; i < rawInput.Length - 1; i++)
-{
-    Console.WriteLine(rawInput[i]);
-    var line = rawInput[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
-
-    for (var j = 0; j < line.Count; j++)
+List<long> nums = [];
+for (int i = rawInput[0].Length - 1; i >= 0; i--)
+{    
+    for (int j = 0; j < rawInput.Length; j++)
     {
-        columns[j].Value.Add(line[j]);
+        if ( j < rawInput.Length -1)
+        {
+            rotatedInput[i] += rawInput[j][i];
+        }
+        else 
+        {
+            nums.Add(long.Parse(rotatedInput[i].Trim()));
+            if (rawInput[j][i] != ' ')
+            {
+                columns.Add(new KeyValuePair<char, List<long>>(rawInput[j][i], nums));
+                i--;
+                nums = [];
+            }
+        }
+        
     }
 }
 
@@ -43,45 +48,4 @@ foreach (var col in columns)
     }
 }
 
-
-
-//answer = await Part1(filePath, answer, ranges);
-
 Console.WriteLine($"Day 6 answer: {answer}");
-
-
-static async Task<long> Part1(string filePath, long answer, List<(long, long)> ranges)
-{
-    using (var reader = new StreamReader(filePath))
-    {
-        string? line;
-        bool isFirststage = true;
-        while (isFirststage)
-        {
-            line = await reader.ReadLineAsync();
-            Console.WriteLine(line);
-
-            if (string.IsNullOrEmpty(line))
-            {
-                isFirststage = false;
-                continue;
-            }
-
-            var rangeParts = line.Split('-').Select(long.Parse).ToArray();
-            ranges.Add((rangeParts[0], rangeParts[1]));
-        }
-
-        while ((line = await reader.ReadLineAsync()) != null)
-        {
-            var prodId = long.Parse(line);
-            var isFresh = ranges.Any(r => r.Item1 <= prodId && r.Item2 >= prodId);
-            if (isFresh)
-            {
-                Console.WriteLine($"Fresh product: {prodId}");
-                answer++;
-            }
-        }
-    }
-
-    return answer;
-}
---
+++ Day7/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Day 7");
+Console.WriteLine();
+
+string filePath = "input.txt";
+using StreamReader sr = new(filePath);
+string? line = await sr.ReadLineAsync();
+int splitCount = 0;
+List<int> beamCols = [
+    line!.IndexOf('S')
+];
+
+Console.WriteLine(line);
+
+while ((line = await sr.ReadLineAsync()) != null)
+{
+    List<int> newBeamCols = [.. beamCols];
+    for (int i = 0; i < beamCols.Count; i++)
+    {
+        if (line[beamCols[i]] == '^')
+        {
+            splitCount++;
+            newBeamCols.Remove(beamCols[i]);
+            if (!newBeamCols.Contains(beamCols[i] - 1))
+            {
+                newBeamCols.Add(beamCols[i] - 1);
+            }
+            if (!newBeamCols.Contains(beamCols[i] + 1))
+            {
+                newBeamCols.Add(beamCols[i] + 1);
+            }            
+        }
+    }
+
+    beamCols = newBeamCols;
+}
+
+Console.WriteLine($"Day 7 answer: {splitCount}");
